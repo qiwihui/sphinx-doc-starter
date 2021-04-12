d3689ad7c18cadcae17d91f981bbe09675e9d07e
qiwihui <qwh005007@gmail.com>
2021-04-12 15:28:17 +0800

    chore: try default engine

--- source/conf.py
+++ source/conf.py
@@ -108,7 +108,7 @@ htmlhelp_basename = 'sphinxdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
-latex_engine = 'xelatex'
+# latex_engine = 'xelatex'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
